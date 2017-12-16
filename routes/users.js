d7a0bdd92f2f7fc6598978a190dbe93c97bd97a8
ahmajor <aggieade@gmail.com>
2017-12-15 18:39:04 -0700

    Migrated personal website from an HTML5 Boilerplate project to a Node.js Express project.

---
+++ routes/users.js
@@ -0,0 +1,13 @@
+/* route used when "/users/" URL is received
+ * (ie, http://localhost:80/users/)
+ */
+
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+module.exports = router;
